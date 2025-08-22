d027bfd1ad33db4cd0c20211367cb2ce0ac4c678
Mathew Goldsborough <mat@nimblebrain.ai>
2025-08-22 13:55:47 -1000

    fixing broken links and urls

---
+++ src/config/externalLinks.js
@@ -0,0 +1,19 @@
+export const externalLinks = {
+  // Hubspot scheduling link
+  schedulingLink:
+    "https://meetings.hubspot.com/nimblebrain/nimbletools-discovery",
+
+  // Documentation
+  docs: "https://docs.nimbletools.ai",
+  docsGettingStarted: "https://docs.nimbletools.ai/getting-started",
+
+  // Linkedin
+  linkedinNimbleBrain: "https://www.linkedin.com/company/nimblebrain/",
+  linkedinNimbleTools: "https://www.linkedin.com/company/nimbletools/",
+
+  // GitHub
+  githubOrg: "https://github.com/NimbleBrainInc",
+  githubNtcli: "https://github.com/NimbleBrainInc/ntcli",
+
+  community: "https://www.nimbletools.ai/slack",
+};
