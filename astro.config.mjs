cbb0bbd2cc8f5b5da2c1bbf4112cc6517cc1fce3
Mathew Goldsborough <mat@nimblebrain.ai>
2025-08-24 16:18:30 -1000

    pagespeed updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nimbletools.ai',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: 'terser',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['astro']
+          }
+        }
+      }
+    }
+  },
+  build: {
+    inlineStylesheets: 'never'
+  }
 });
