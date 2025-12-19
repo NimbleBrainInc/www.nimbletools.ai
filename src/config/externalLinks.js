50f318c993cd43a1f7f747ef6afea32b225258df
Mathew Goldsborough <mat@nimblebrain.ai>
2025-12-19 08:59:06 -1000

    cleanup - simple landing page to reflect oss runtime

--- src/config/externalLinks.js
+++ src/config/externalLinks.js
@@ -1,26 +1,14 @@
 export const externalLinks = {
-  // Hubspot scheduling link
-  schedulingLink:
-    "https://meetings.hubspot.com/nimblebrain/nimbletools-discovery",
-
-  // Documentation
-  docs: "https://docs.nimbletools.ai",
-  docsGettingStarted: "https://docs.nimbletools.ai/getting-started",
-
-  // Linkedin
-  linkedinNimbleBrain: "https://www.linkedin.com/company/nimblebrain/",
-  linkedinNimbleTools: "https://www.linkedin.com/company/nimbletools/",
+  // Documentation - NimbleBrain docs
+  docs: "https://docs.nimblebrain.ai/runtime/introduction",
 
   // GitHub
   githubOrg: "https://github.com/NimbleBrainInc",
   githubCore: "https://github.com/NimbleBrainInc/nimbletools-core",
-  githubNtcli: "https://github.com/NimbleBrainInc/ntcli",
 
-  // App and Payment links
-  appLink: "https://app.nimbletools.ai",
-  starterPayment: "https://buy.stripe.com/4gM5kvbxA6Tr74sfn038400",
-  teamPayment: "https://buy.stripe.com/bJe7sD7hka5D4Wkgr438402",
+  // Parent company
+  nimblebrain: "https://www.nimblebrain.ai",
 
-  slack: "/slack",
+  // Community
   discord: "/discord",
 };
